29d0194dfd43659c84d28534be21c11a12ed4a80
j8130 <122969720@qq.com>
2020-10-12 00:38:23 +0800

    理论学习完毕

--- src/c12/FeatureSelection.scala
+++ src/c12/FeatureSelection.scala
@@ -1,30 +1,37 @@
 package c12
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.mllib.feature.ChiSqSelector
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+ * 卡方检验
+ * 卡方检验主要是对已有向量进行数据归类处理
+ */
 object FeatureSelection {
   def main(args: Array[String]) {
-val conf = new SparkConf()                                   	//创建环境变量
-.setMaster("local")                                          	//设置本地化处理
-.setAppName("FeatureSelection ")                              //设定名称
-val sc = new SparkContext(conf)                               	//创建环境变量实例
-    val data = MLUtils.loadLibSVMFile(sc, "c://fs.txt")				//读取数据文件
-    
-    val discretizedData = data.map { lp =>						//创建数据处理空间
-      LabeledPoint(lp.label, Vectors.dense(lp.features.toArray.map { x => x / 2 } ) )
+    val conf = new SparkConf() //创建环境变量
+      .setMaster("local") //设置本地化处理
+      .setAppName("FeatureSelection ") //设定名称
+    val sc = new SparkContext(conf) //创建环境变量实例
+    Logger.getRootLogger.setLevel(Level.ERROR)
+
+    val data = MLUtils.loadLibSVMFile(sc, "E:\\GitHub\\sparkMLlib-learn\\resources\\DATA\\D12\\FeatureSelection.txt") //读取数据文件
+
+    val discretizedData = data.map { lp => //创建数据处理空间
+      LabeledPoint(lp.label, Vectors.dense(lp.features.toArray.map { x => x / 2 }))
     }
 
-    val selector = new ChiSqSelector(2)					//创建选择2个特性的卡方检验实例
-    val transformer = selector.fit(discretizedData)					//创建训练模型
-    val filteredData = discretizedData.map { lp =>					//过滤前2个特性
+    val selector = new ChiSqSelector(2) //创建选择2个特性的卡方检验实例
+    val transformer = selector.fit(discretizedData) //创建训练模型
+    val filteredData = discretizedData.map { lp => //过滤前2个特性
       LabeledPoint(lp.label, transformer.transform(lp.features))
     }
 
-    filteredData.foreach(println)								//打印结果
+    filteredData.foreach(println) //打印结果
   }
 }
 
